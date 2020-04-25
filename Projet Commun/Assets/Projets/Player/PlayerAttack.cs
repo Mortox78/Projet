dc9ef119cac3ce4fc2d088cd2e359605159b2539
Steeven <steeven_yugo@hotmail.fr>
2020-04-25 13:33:20 +0200

    Add attack system, add player changing look direction

---
+++ Projet Commun/Assets/Projets/Player/PlayerAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackSpeed = 1f;
+
+    private float attackTimer = 0f;
+    private bool  isAttack = false;
+    private BoxCollider2D attackCollider = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackCollider = GetComponent<BoxCollider2D>();
+        if (attackCollider != null)
+        {
+            attackCollider.isTrigger = true;
+            attackCollider.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("attackCollider == null");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAttack && Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            isAttack = true;
+            attackCollider.enabled = true;
+        }
+        if (isAttack)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= attackSpeed)
+            {
+                attackTimer = 0f;
+                isAttack = false;
+                attackCollider.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Projet Commun/Assets/Projets/Player/PlayerMovement.cs
+++ Projet Commun/Assets/Projets/Player/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float gravitation = 1f;
 
 
+    private float rayCastHitDistance = 1.1f;
     private bool isJump = false;
     private bool isFall = false;
     private Rigidbody2D rb = null;
@@ -23,50 +24,62 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Assert our variables are > 0
+        if (horizontalSpeed <= 0f || jumpPower <= 0f || gravitation <= 0f)
+            Debug.LogError("ERROR : some variables are <= 0f");
+
+        // Horizontal movement
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(0f, rb.velocity.y) + Vector2.left * horizontalSpeed;
+
+            // Change player's direction
+            if (transform.localScale.x > 0f)
+                transform.localScale = new Vector3(-1f * transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(0f, rb.velocity.y) + Vector2.right * horizontalSpeed;
+
+            // Change player's direction
+            if (transform.localScale.x < 0f)
+                transform.localScale = new Vector3(-1f * transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
         else
         {
             rb.velocity = new Vector2(0f, rb.velocity.y);
         }
 
+        // Vertical movement
         if (!isJump && Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("START TO JUMPPPP");
             isJump = true;
             rb.velocity += new Vector2(0f, jumpPower);
         }
         else if (isJump)
         {
-            Debug.Log("IS JUMPINNNGGGG");
             rb.velocity -= new Vector2(0f, gravitation);
-            if (rb.velocity.y <= 0)
+            if (!isFall && rb.velocity.y <= 0) 
                 isFall = true;
         }
 
+        // Ground check with raycast2D
         int layerMask = 1 << 8;
         layerMask = ~layerMask;
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 1.1f, layerMask);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, rayCastHitDistance, layerMask);
 
         if (hit && isFall)
         {
-            Debug.DrawRay(transform.position, -Vector2.up * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, -Vector2.up * hit.distance, Color.blue, 3f);
             //Debug.Log("Hit the gameObject : " + hit.collider.name + "  AND OF COURSE DISTANCE : " + hit.distance);
-            Debug.Log("STOP TO JUMP");
             isFall = false;
             isJump = false;
             rb.velocity = new Vector2(rb.velocity.x, 0f);
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * 1000, Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(-Vector3.up) * 1000, Color.red);
             //Debug.Log("Did not Hit");
         }
     }
