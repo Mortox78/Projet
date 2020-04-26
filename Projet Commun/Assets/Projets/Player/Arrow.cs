207544f52cbd31567eb0b279a762b4e4d65625b5
Steeven <steeven_yugo@hotmail.fr>
2020-04-26 15:18:49 +0200

    Add shoot arrows system

---
+++ Projet Commun/Assets/Projets/Player/Arrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class Arrow : MonoBehaviour
+{
+    [System.NonSerialized] public Rigidbody2D rb = null;
+
+    private float gravitation = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.up = rb.velocity.normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Projet Commun/Assets/Projets/Player/PlayerBowAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBowAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject arrow = null;
+    [SerializeField] private float bowPower = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Camera cam = FindObjectOfType<Camera>();
+            Vector2 playerScreenPos = cam.WorldToScreenPoint(this.transform.position);
+            Vector2 mousePos = Input.mousePosition;
+
+            Vector2 playerToMouse = mousePos - playerScreenPos;
+
+            GameObject arrowShot = Instantiate(arrow);
+            Arrow arrowShotScript = GetComponent<Arrow>();
+            arrowShot.transform.position = this.transform.position;
+            arrowShot.GetComponent<Rigidbody2D>().velocity = playerToMouse.normalized * bowPower;
+        }
+    }
+}
